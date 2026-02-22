f900aab31c432f7f0f0c49b53c0fa14673ff9c6b
nkraemer <kraemer.nicholas@gmail.com>
2026-02-22 14:26:44 -0800

    template commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref<number>(0)
+</script>
+
+<template>
+  <div class="counter">
+    <button @click="count--">-</button>
+    <span>Count: {{ count }}</span>
+    <button @click="count++">+</button>
+  </div>
+</template>
+
+<style scoped>
+.counter {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  font-size: 1.25rem;
+}
+
+button {
+  width: 2rem;
+  height: 2rem;
+  font-size: 1.25rem;
+  cursor: pointer;
+}
+</style>
