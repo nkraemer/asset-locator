63c466f99203b77820499a462d9ccd6163c562dd
Nicholas <Nicholas Kraemer>
2026-07-08 20:58:26 -0700

    reverted base to '/'

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/asset-locator/',
+  base: '/',
   test: {
     environment: 'jsdom',
     globals: true,
